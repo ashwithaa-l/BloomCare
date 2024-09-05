0ebb5f2d9a5775466e5796296784f5c301559879
aswathcm29 <aswathcm29@gmail.com>
2024-09-05 23:00:51 +0530

    fixed assets

--- frontend/src/components/Esanjeevini.jsx
+++ frontend/src/components/Esanjeevini.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import backgroundImage from '../assets/esanjeev.jpg'; 
 
 const Esanjeevini = () => {
   return (
     <div
       className="relative min-h-screen bg-cover bg-center"
-      style={{ backgroundImage: 'url(/esanjeev.jpg)' }}
+      style={{ backgroundImage: `url(${backgroundImage})` }}
     >
       <div className="absolute bottom-16 left-1/2 transform -translate-x-1/2">
         <button className="bg-blue-500 text-white px-8 py-4 rounded-lg shadow-md text-xl hover:bg-blue-600">
